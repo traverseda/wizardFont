116d6fefb6b5514086d0c6f69c2cd1c0cbc4203d
Alex Davies <traverse.da@gmail.com>
2018-07-25 12:45:00 -0300

    Initial commit

---
+++ compile.py
@@ -0,0 +1,15 @@
+from itertools import count
+counter = count()
+
+import yaml
+conf = open("font.yaml", 'r')
+conf = yaml.load(conf)
+
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('./'))
+template = env.get_template('cscriptFont.svg.j2')
+output_from_parsed_template = template.render(id=lambda:counter.__next__(),**conf)
+
+# to save the results
+with open("cscriptFont.svg", "wb") as fh:
+    fh.write(output_from_parsed_template.encode('utf-8'))
